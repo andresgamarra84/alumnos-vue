0492c0ec4c4e445dfd9b7c350babab6567feeecd
Andrés Gamarra <andresgamarra@gmail.com>
2026-04-24 18:20:35 -0300

    Solicitud de mesa de examen

--- src/views/estudiantes/sections/tramites/SolicitudMesaView.vue
+++ src/views/estudiantes/sections/tramites/SolicitudMesaView.vue
@@ -25,3 +25,123 @@
 		<div v-for='(item,key) in arrProf' v-on:click='selProf(key)' class='lista' style='border:1px solid white; margin:10px;padding:10px;'>{{item.contenido}}<br>Prof: {{item.nombreProf}}</div>
 	</div>
 </template>
+<script setup>
+import { ref, onMounted } from 'vue';
+import { api } from '@/api/api.js';
+import { showModal } from '@/services/uiBus';
+import CarrerasSelect from '@/components/CarrerasSelect.vue';
+import MateriasSelect from '@/components/MateriasSelect.vue';
+const carreras = ref([]);
+const materias = ref([]);
+const mesasDisponibles = ref([]);
+const selectedCarrera = ref(null);
+const selectedMateria = ref('');
+const selectedCondicion = ref(null);
+const codMesa = ref(null);
+const condicionShow = ref(false);
+const condiciones = ref([]);
+/* ---------- lifecycle ---------- */
+
+onMounted(() => {
+  getCarrerasAlumno();
+  getCondiciones();
+});
+
+/* ---------- handlers ---------- */
+const getCarrerasAlumno = async () => {
+  try {
+    const r = await api.get({
+      entity: 'carreras',
+      action: 'getCarrerasAlumno',
+    });
+    carreras.value = r.payload ?? [];
+  } catch (e) {
+    console.log(e);
+  } 
+};
+const getCondiciones = async () => {
+  const r = await api.get({
+    entity: "examenes",
+    action: "getCondicionesExamen",
+  });
+  condiciones.value = r.payload ?? [];
+}
+const onSelectCarrera = async () => {
+  selectedMateria.value = '';
+  codMesa.value = null;
+  condicionShow.value = false;
+  materias.value = [];
+  mesasDisponibles.value = [];
+  try {
+    const codAlC = carreras.value[selectedCarrera.value].codigo;
+    const r = await api.get({
+      entity: 'materias',
+      action: 'getMateriasCarrera',
+      payload: {
+        codigo : codAlC,
+        inscripcionMesa: true
+      },
+    });
+    materias.value = r.payload ?? [];
+  } catch (e) {
+    console.log(e);
+  }
+};
+const onSelectMateria = async () => {
+  codMesa.value = null;
+  condicionShow.value = false;
+  mesasDisponibles.value = [];
+  try {
+    const codAlC = carreras.value[selectedCarrera.value].codigo;
+    const r = await api.get({
+      entity: 'mesas',
+      action: 'listarMesasDisponibles',
+      payload: {
+        codAlCarrera: codAlC,
+        codMC: selectedMateria.value,
+      },
+    });
+    mesasDisponibles.value = r.payload ?? [];
+  } catch (e) {
+    console.log(e);
+  }
+};
+const onSelectMesa = mesa => {
+  codMesa.value = mesa.codigo;
+  condicionShow.value = true;
+};
+const confirmarInscripcion = async () => {
+  const codAlC = carreras.value[selectedCarrera.value].codigo;
+  console.log(selectedCondicion.value)
+  if (selectedCondicion.value === null) {
+    showModal('Debe seleccionar una condición de examen');
+    return;
+  }
+
+  const c = await showModal('¿Confirma inscripción?', 1);
+  if (!c.ok) return;
+
+  const r = await api.post({
+    entity: "mesas",
+    action: "inscribirEnMesa",
+    payload: {
+      codAlC: codAlC,
+      codMC: selectedMateria.value,
+      codMesa: codMesa.value,
+      condicion: selectedCondicion.value,
+    },
+  });
+
+  if (r.ok) {
+    showModal(
+      'La inscripción ha sido realizada y ya puede visualizarse en la página de inicio'
+    );
+  }
+  selectedCarrera.value = '';
+  selectedMateria.value = '';
+  selectedCondicion.value = null;
+  codMesa.value = null;
+  condicionShow.value = false;
+
+};
+</script>
